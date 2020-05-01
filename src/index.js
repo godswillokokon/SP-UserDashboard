16bd27f528b405f710c0785f9446eee8b91579c7
ThaTruth24 <thankgodegbo@gmail.com>
2020-05-01 17:50:28 +0100

    Trying Fixes

--- src/index.js
+++ src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-// import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { store, persistor } from "./store";
-// import LoaderBox from "./components/LoaderBox";
 
-const Root = () => (
-	// <Provider store={store}>
-	// 	<PersistGate persistor={persistor} loading={<LoaderBox />}>
-	<App />
-	// 	</PersistGate>
-	// </Provider>
-);
+const Root = () => {
+	return <App />;
+};
 
 ReactDOM.render(<Root />, document.getElementById("root"));
 
